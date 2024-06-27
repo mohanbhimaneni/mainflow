dcb70bd824f2a8b49659b8bc48851adcc9c30f71
mohanbhimaneni <129185293+mohanbhimaneni@users.noreply.github.com>
2024-06-27 15:25:01 +0530

    Update task_5_Main_flow.py

--- task_5_Main_flow.py
+++ task_5_Main_flow.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
 df=pd.read_csv("C:/Users/bhmoh/Downloads/heart.csv")
 print(df.head())
 print(df.tail())
